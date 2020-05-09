c54dfbf5e3faebee2ca9d8d6b24e5c015f78bb0e
auttij <juhana.autti@gmail.com>
2020-05-09 14:52:18 +0300

    Added console log to exercise es10 in section 13

--- section13/advanced_exercise_es10/ES10-Exercise.js
+++ section13/advanced_exercise_es10/ES10-Exercise.js
@@ -37,3 +37,5 @@ const newUsers = uArr.map((user) => [user[0], user[1] * 2])
 console.log(newUsers);
 
 //#8 change the output array of question #7 back into an object with all the users IDs updated to their new version. Should output: { user1: 36546, user2: 185666, user3: 180630 }
+const newObj = Object.fromEntries(newUsers);
+console.log(newObj);
